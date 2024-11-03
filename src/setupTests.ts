0f0df92003f5f498564293f109cb200697e12fd9
Ilia Kamilov <iliakmlv@gmail.com>
2024-11-03 23:04:33 +0200

    fix: Error: src/setupTests.ts(7,1): error TS2304: Cannot find name 'afterEach'.

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,6 @@
 import '@testing-library/jest-dom';
 import { cleanup } from '@testing-library/react';
+import { afterEach } from 'vitest';
 // import matchers from '@testing-library/jest-dom/matchers';
 
 // expect.extend(matchers);
